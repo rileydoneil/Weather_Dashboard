64d46f0f35ff28cafd74087139d625832481bdbc
Riley O'Neil <roneilwork@gmail.com>
2023-03-21 16:36:12 -0700

    fixed main card issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,6 +19,7 @@ searchBTN.addEventListener('click', function(event) {
         console.log("Heres the city to data");
         console.log(response[0]);
         getWeather5day(response[0]);
+        getWeather(response[0]);
       })
       .catch(err => console.error(err));
 
@@ -31,13 +32,38 @@ const getWeather5day = function(data) {
     fetch('https://api.openweathermap.org/data/2.5/forecast?lat=' + data.lat + '&lon=' + data.lon + '&appid=' + apiKey + '&units=imperial')
       .then(response => response.json())
       .then(response => {
-        console.log("Heres the data from Weather: ");
+        console.log("Heres the data from Weather 5 day: ");
         console.log(response);
         createCards(response);
       })
       .catch(err => console.error(err));
-
 }
+const getWeather = function(data) {
+    // api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+    // fetch('api.openweathermap.org/data/2.5/forecast?lat=' + data.lat + '&lon=' + data.lon + '&appid=' + apiKey)
+    fetch('https://api.openweathermap.org/data/2.5/weather?lat=' + data.lat + '&lon=' + data.lon + '&appid=' + apiKey + '&units=imperial')
+      .then(response => response.json())
+      .then(response => {
+        console.log("Heres the data from Weather 1 day: ");
+        console.log(response);
+        createMainCard(response);
+      })
+      .catch(err => console.error(err));
+}
+
+const createMainCard = function(data) {
+    let objMain = data.main;
+    let objWind = data.wind;
+    
+    let avgWind = objWind.speed;
+    let avgTemp = objMain.temp;
+    let avgHum = objMain.humidity;
+    console.log("Here is wind, temp, hum for main Card:" + avgWind + " " + avgHum + " " + avgTemp);
+    document.querySelector('.temp').innerHTML += avgTemp.toFixed(2) + '°F';
+    document.querySelector('.wind').innerHTML += avgWind.toFixed(2) + 'MPH';
+    document.querySelector('.humidity').innerHTML += avgHum.toFixed(2) + '%';
+}
+
 
 //populate weather cards
 const createCards = function(data) {
@@ -56,25 +82,18 @@ const createCards = function(data) {
                 avgHum += objMain.humidity / 8;
             }
             console.log("Here is the mintemp: " + avgTemp + "and humidity" + avgHum );
-            if (i == 0) {
-                // https://stackoverflow.com/questions/3163070/javascript-displaying-a-float-to-2-decimal-places
-                document.querySelector('.temp').innerHTML += avgTemp.toFixed(2) + '°F';
-                document.querySelector('.wind').innerHTML += avgWind.toFixed(2) + 'MPH';
-                document.querySelector('.humidity').innerHTML += avgHum.toFixed(2) + '%';
-            } else {
-                let arr = [avgTemp, avgWind, avgHum];
-                console.log(arr);
+            let arr = [avgTemp, avgWind, avgHum];
+            console.log(arr);
 
-                const dayCard = document.createElement('div');
-                dayCard.classList.add('dayCard');
-                dayCard.innerHTML = `
-                <h2>Date</h2>
-                <h5>Temp: ${avgTemp.toFixed(2) + '°F'}</h5>
-                <h5>Wind: ${avgWind.toFixed(2) + 'MPH'}</h5>
-                <h5>Humidity: ${avgHum.toFixed(2) + '%'}</h5>
-                `
-                weekForecast.appendChild(dayCard);
-            }
+            const dayCard = document.createElement('div');
+            dayCard.classList.add('dayCard');
+            dayCard.innerHTML = `
+            <h2>Date</h2>
+            <h5>Temp: ${avgTemp.toFixed(2) + '°F'}</h5>
+            <h5>Wind: ${avgWind.toFixed(2) + 'MPH'}</h5>
+            <h5>Humidity: ${avgHum.toFixed(2) + '%'}</h5>
+            `
+            weekForecast.appendChild(dayCard);
             i += 8;
         }
 }
